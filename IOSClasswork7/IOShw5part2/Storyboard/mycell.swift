23260d04425813e5c09fa2a1350408991df36af4
Mona Alrashidi <63103306+MonaAlrashidi@users.noreply.github.com>
2020-07-09 23:22:35 +0300

    hw5 part2

---
+++ IOSClasswork7/IOShw5part2/Storyboard/mycell.swift
@@ -0,0 +1,26 @@
+//
+//  mycell.swift
+//  IOSClasswork7
+//
+//  Created by Badria Alqanai on 7/9/20.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import UIKit
+
+class mycell: UITableViewCell {
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var movieLabel: UILabel!
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
